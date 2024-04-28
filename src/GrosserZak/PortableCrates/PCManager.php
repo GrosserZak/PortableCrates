ebb602c5aeaf5dc158cebf0cdec175a9d378f089
XyloFD <104400758+XyloFD2@users.noreply.github.com>
2024-04-28 17:58:51 -0400

    Another Possible Fix smh.

--- src/GrosserZak/PortableCrates/PCManager.php
+++ src/GrosserZak/PortableCrates/PCManager.php
@@ -88,7 +88,7 @@ class PCManager {
                     $item = StringToItemParser::getInstance()->parse($reward[0]);
 
         if ($item !== null) {
-            $item->setCount($count <= 64 ? $count : 1);
+            $item->setCount($count <= 64 ? $count : 1)
              ->setCustomName(G::RESET . G::WHITE . "x" . $count . " ". $reward[2]);
              ->setLore(array_merge($reward[3], ["", G::RESET . G::GREEN . $reward[5] . "% probability"]));
                 }
